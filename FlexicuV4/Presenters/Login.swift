c7ed9af4477b8d51bc08bb05a453a0ff4753e876
Gunn Hentze <gunnhentze@gunns-macbook-pro.local>
2018-12-04 18:22:39 +0100

    Opret bruger er i orden

--- FlexicuV4/Presenters/Login.swift
+++ FlexicuV4/Presenters/Login.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class Login: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginBtnPressed: UIButton!
     override func viewDidLoad() {
+   //     var ref: DatabaseReference
+     //   ref = Database.database().reference()
+       // ref.child("hey").child("1").setValue("Gunn")
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -31,14 +37,27 @@ class Login: UIViewController {
         print(UIDevice.current.orientation.isLandscape)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func LoginPressed(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if(error != nil){
+                print(error!)
+            }
+            else{
+                print("Du er logget ind")
+                
+                
+                let tabBar = self.storyboard?.instantiateViewController(withIdentifier: "tabBar") as! UITabBarController
+                
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                
+                appDelegate.window?.rootViewController = tabBar
+                
+//                let tabBarCV = self.storyboard?.instantiateViewController(withIdentifier: "tabBarController") as! TabBarController
+//                self.navigationController?.pushViewController(tabBarCV, animated: true)
+            }
+        }
+        
     }
-    */
-
+    
 }
