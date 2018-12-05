578fea1b1a3d3b12b6b4bc064979a3e43a08d5ca
Gunn Hentze <gunnhentze@gunns-macbook-pro.local>
2018-12-05 17:59:24 +0100

    pdf

--- FlexicuV4/Model/Virksomhed.swift
+++ FlexicuV4/Model/Virksomhed.swift
@@ -14,18 +14,18 @@ class Virksomhed{
     var virkNavn:String
     var virkAdresse:String
     var postnr:String
-//    var id:String
+    //    var id:String
     var brugNavn:String
     var brugEmail:String
     var brugTlf:String
     var medarbejdere: Array<Medarbejder> = Array()
     
-    init(CVR: String , virkNavn:String, virkAdresse: String, postnr:String, /*id: String,*/ brugNavn:String, brugEmail:String, brugTlf:String){   
+    init(CVR: String , virkNavn:String, virkAdresse: String, postnr:String, /*id: String,*/ brugNavn:String, brugEmail:String, brugTlf:String){
         self.CVR = CVR
         self.virkNavn = virkNavn
         self.virkAdresse = virkAdresse
         self.postnr = postnr
-//        self.id = id
+        //        self.id = id
         self.brugNavn = brugNavn
         self.brugEmail = brugEmail
         self.brugTlf = brugTlf
