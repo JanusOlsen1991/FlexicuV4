687d03e4736ad085bd3b5bb611304ccbb698f5c9
Janus Olsen <janus-olsen91@hotmail.com>
2018-12-07 13:51:37 +0100

    Layout næsten færdig + HexfarveKlasse

---
+++ FlexicuV4/CollectionviewCeller/SoegtArbejdskraftCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  SoegtArbejdskraftCVCell.swift
+//  FlexicuV4
+//
+//  Created by Janus Olsen on 07/12/2018.
+//  Copyright © 2018 DTu Diplom. All rights reserved.
+//
+
+import UIKit
+
+class SoegtArbejdskraftCVCell: UICollectionViewCell {
+    @IBOutlet weak var NavnLabel: UILabel!
+    
+    @IBOutlet weak var PrisLabel: UILabel!
+    @IBOutlet weak var LedigIPeriodeLabel: UILabel!
+    @IBOutlet weak var arbejdsomraadeLabel: UILabel!
+}
--- FlexicuV4/Presenters/Indstillinger.swift
+++ FlexicuV4/Presenters/Indstillinger.swift
@@ -1,15 +1,23 @@
 //
-//  Indbakke.swift
-//  FlexicuV2
+//  Indstillinger.swift
+//  FlexicuV4
 //
-//  Created by Janus Olsen on 14/11/2018.
-//  Copyright © 2018 Student. All rights reserved.
+//  Created by Janus Olsen on 07/12/2018.
+//  Copyright © 2018 DTu Diplom. All rights reserved.
 //
 
 import UIKit
 
 class Indstillinger: UIViewController {
-
+    @IBOutlet weak var CVRtxt: UITextField!
+    @IBOutlet weak var vshNavntxt: UITextField!
+    @IBOutlet weak var vshAdressetxt: UITextField!
+    
+    @IBOutlet weak var eMailTxt: UITextField!
+    @IBOutlet weak var brugersTitelTxt: UITextField!
+    @IBOutlet weak var brugersTlfTxt: UITextField!
+    @IBOutlet weak var postNrtxt: UITextField!
+    @IBOutlet weak var brugersNavnTxt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +29,10 @@ class Indstillinger: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func logAfAction(_ sender: Any) {
+    //TODO -> gåtilbage til login skærm, samt data der bruges i programmet tømmes?
+    }
+    
     /*
     // MARK: - Navigation
 
